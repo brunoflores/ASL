73d8dd652b95d503734fdb7087780d55e149a9af
Bruno Flores <bruno@brunoflores.com.br>
2023-11-26 13:37:06 +1000

    Test.

--- lib/cam.ml
+++ lib/cam.ml
@@ -139,27 +139,29 @@ let init_cam_env =
 
 let global_cam_env = ref init_cam_env
 
-let%test _ =
-  let ast = Ast.Const 1 in
-  let got = code_of ast in
-  let want = [ Quote 1 ] in
-  equal_instr_list got want
-
-let%test_unit _ =
-  let want =
-    [ Push; Push; Nth 6; Swap; Quote 1; Apply; Swap; Quote 2; Apply ]
-  in
-  let ast = Ast.App (App (Var 6, Const 1), Const 2) in
-  let got = code_of ast in
-  if not (equal_instr_list got want) then (
+let test_it ast instrs =
+  let code = code_of ast in
+  if not (equal_instr_list code instrs) then (
     print_endline
-    @@ Format.sprintf "\nwant: %s\ngot: %s\n\n" (show_instr_list want)
-         (show_instr_list got);
+    @@ Format.sprintf "\nwant: %s\ngot: %s\n\n" (show_instr_list instrs)
+         (show_instr_list code);
     raise (Failure ""))
   else ()
 
 let%test_unit _ =
-  let want =
+  let ast = Ast.Const 1 in
+  let instrs = [ Quote 1 ] in
+  test_it ast instrs
+
+let%test_unit _ =
+  let instrs =
+    [ Push; Push; Nth 6; Swap; Quote 1; Apply; Swap; Quote 2; Apply ]
+  in
+  let ast = Ast.App (App (Var 6, Const 1), Const 2) in
+  test_it ast instrs
+
+let%test_unit _ =
+  let instrs =
     [
       Push;
       Clos [ Nth 1 ];
@@ -173,16 +175,10 @@ let%test_unit _ =
     ]
   in
   let ast = Ast.App (Abs ("x", Var 1), App (Abs ("x", Var 1), Const 0)) in
-  let got = code_of ast in
-  if not (equal_instr_list got want) then (
-    print_endline
-    @@ Format.sprintf "\nwant: %s\ngot: %s\n\n" (show_instr_list want)
-         (show_instr_list got);
-    raise (Failure ""))
-  else ()
+  test_it ast instrs
 
 let%test_unit _ =
-  let want =
+  let instrs =
     [
       Push;
       Push;
@@ -198,10 +194,4 @@ let%test_unit _ =
     ]
   in
   let ast = Ast.App (App (Var 6, Const 1), Cond (Const 0, Const 2, Const 3)) in
-  let got = code_of ast in
-  if not (equal_instr_list got want) then (
-    print_endline
-    @@ Format.sprintf "\nwant: %s\ngot: %s\n\n" (show_instr_list want)
-         (show_instr_list got);
-    raise (Failure ""))
-  else ()
+  test_it ast instrs
